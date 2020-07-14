d83f0021c78cde8a0e0e0f188edb54a1fd2680ef
jmin <j0102276@gmail.com>
2020-07-14 10:33:26 +0900

    회원 포인트 관리 페이징 수정

--- src/main/java/com/webapp/manager/dao/MgPointDAO.java
+++ src/main/java/com/webapp/manager/dao/MgPointDAO.java
@@ -1,5 +1,6 @@
 package com.webapp.manager.dao;
 
+import com.webapp.board.common.SearchVO;
 import com.webapp.mall.dao.PointDAO;
 import com.webapp.manager.vo.MgPointVO;
 import org.apache.ibatis.session.SqlSession;
@@ -18,7 +19,6 @@ public class MgPointDAO {
         List<Map<String, Object>> getMgPointList = sql.selectList("mall.MgPointMapper.getMgPointList",MgPointVO);
         return getMgPointList;
     }
-
     public Integer getMgPointListCount(MgPointVO MgPointVO) throws SQLException {
         Integer getMgPointListCount=sql.selectOne("mall.MgPointMapper.getMgPointListCount",MgPointVO);
         return getMgPointListCount;
